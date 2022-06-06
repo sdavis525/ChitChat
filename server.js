fee0a62245ddaa069d05e181ea79938400a0d7ec
Sophia Davis <sophiadavis@Sophias-MacBook-Pro.local>
2022-06-06 12:22:41 -0400

    server.js completed

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
+const routes = require('./routes');
+const app = express();
+
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
 
-const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -21,6 +22,9 @@ const sess = {
 
 app.use(session(sess));
 
+
+const PORT = process.env.PORT || 3001;
+
 const helpers = require('./utils/helpers');
 
 const hbs = exphbs.create({ helpers });
@@ -29,10 +33,10 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
